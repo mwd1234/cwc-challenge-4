6238d3cf1286df1cc40d3c10f11d04f03e30b317
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-30 14:31:50 -0700

    Initial Commit

---
+++ CWC Challenge 4/CWC_Challenge_4App.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_Challenge_4App.swift
+//  CWC Challenge 4
+//
+//  Created by Matthew Dembinski on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CWC_Challenge_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC Challenge 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CWC Challenge 4
+//
+//  Created by Matthew Dembinski on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
